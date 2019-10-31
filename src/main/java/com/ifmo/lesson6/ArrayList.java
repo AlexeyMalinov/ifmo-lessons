33b032d184cdea82e1117213445db7262f9d59b2
Alexey Malinov <alexeymalinov@yandex.ru>
2019-10-31 10:09:11 +0300

    homework6 done

--- src/main/java/com/ifmo/lesson6/ArrayList.java
+++ src/main/java/com/ifmo/lesson6/ArrayList.java
@@ -96,6 +96,6 @@ public class ArrayList implements List {
      * Метод расширяет внутренний массив в два раза
      */
     private void reSize() {
-        Arrays.copyOf(values, values.length * 2);
+        values = Arrays.copyOf(values, values.length * 2);
     }
 }
