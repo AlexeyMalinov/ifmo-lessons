5abe7c67f9b7024a0b6da1ebdab756334fa5d6fc
Alexey Malinov <alexeymalinov@yandex.ru>
2019-11-14 13:07:05 +0300

    homework9 done

--- src/main/java/com/ifmo/lesson9/DoubleAppendable.java
+++ src/main/java/com/ifmo/lesson9/DoubleAppendable.java
@@ -2,28 +2,14 @@ package com.ifmo.lesson9;
 
 public class DoubleAppendable extends AbstractNumberAppendable<Double, DoubleAppendable> {
 
-    Double value;
-
     private static final ArithmeticOperation<Double> DEFAULT_OPERATION = Double::sum;
 
     public DoubleAppendable(Double value, ArithmeticOperation op) {
-        super(op);
-        this.value = value;
+        super(value, op);
     }
 
     public DoubleAppendable(){
-        super( DEFAULT_OPERATION);
+        super(0D, DEFAULT_OPERATION);
     }
 
-
-    @Override
-    public DoubleAppendable append(Double type) {
-        value = this.op.apply(value, type);
-        return this;
-    }
-
-    @Override
-    public Double value() {
-        return value;
-    }
 }
--- src/main/java/com/ifmo/lesson9/IntAppendable.java
+++ src/main/java/com/ifmo/lesson9/IntAppendable.java
@@ -1,34 +1,15 @@
 package com.ifmo.lesson9;
 
 public class IntAppendable extends AbstractNumberAppendable <Integer, IntAppendable> {
-    int value;
 
     private static ArithmeticOperation<Integer> DEFAULT_OPERATION = Integer::sum;
 
-    public IntAppendable(ArithmeticOperation<Integer> op) {
-        super(op);
-        this.value = value;
+    public IntAppendable(Integer value, ArithmeticOperation<Integer> op) {
+        super(value, op);
     }
 
     public IntAppendable(){
-        super(DEFAULT_OPERATION);
+        super(0, DEFAULT_OPERATION);
     }
-
-    @Override
-    public IntAppendable append(Integer type) {
-        value = this.op.apply(value, type);
-        return this;
-    }
-
-    @Override
-    public Integer value() {
-        return value;
-    }
-
-    public static void main(String[] args) {
-        Appendable<Integer, IntAppendable> appendable = new IntAppendable();
-        appendable.append(1).value();
-    }
-
 
 }
