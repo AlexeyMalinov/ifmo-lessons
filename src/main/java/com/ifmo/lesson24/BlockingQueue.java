f9bd1376b0e864348e7b5e546910d93deae732f1
Alexey Malinov <alexeymalinov@yandex.ru>
2019-12-14 12:15:58 +0300

    BlockingQueue.java update

--- src/main/java/com/ifmo/lesson24/BlockingQueue.java
+++ src/main/java/com/ifmo/lesson24/BlockingQueue.java
@@ -1,33 +1,38 @@
 package com.ifmo.lesson24;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class BlockingQueue<T> {
 
     private final List<T> list = new ArrayList<>();
     private final Object mutex = new Object();
-
+    private AtomicInteger index = new AtomicInteger(0);
 
     public boolean add(T t) {
-        if(list.size() == 0){
+        if(index.get() < 1){
             synchronized (mutex){
                 list.add(t);
+                index.incrementAndGet();
                 mutex.notify();
             }
             return true;
         }
+        index.incrementAndGet();
         return list.add(t);
     }
 
     public T take() throws InterruptedException {
-        if(list.size() <= 1){
+        if(index.get() < 1){
             synchronized (mutex){
-                while(list.size() == 0) {
+                while(index.get() < 1) {
                     mutex.wait();
                 }
+                index.decrementAndGet();
                 return list.remove(0);
             }
         }
+        index.decrementAndGet();
         return list.remove(0);
     }
 
